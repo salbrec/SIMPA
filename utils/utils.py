3d650000efbbaca44690eb949a3f6d0b5bc74d3f
salbrec <salbrec@uni-mainz.de>
2022-01-06 11:34:18 +0100

    update utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-def get_allowed_chrom_str():
-	return ['chr%d'%(i+1) for i in range(22)]
+def get_allowed_chrom_str(assembly):
+	chroms = None
+	if assembly in ['hg19', 'hg38']:
+		chroms = ['chr%d'%(i+1) for i in range(22)]
+	if assembly in ['mm10']:
+		chroms = ['chr%d'%(i+1) for i in range(19)]
+	return chroms
 
 def get_chrom_sizes(sizes_path):
 	df = pd.read_csv(sizes_path, names=['chrom', 'size'], sep='\t')
